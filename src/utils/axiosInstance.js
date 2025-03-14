5c4783946e9d4e4ebe4c30cef05ff07785a8d949
Anirudhakale98 <anirudhakale982354@gmail.com>
2025-03-15 03:34:43 +0530

    baseURL is added axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: '', // Replace with your backend URL
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080', // Replace with your backend URL
     withCredentials: true, // To include cookies
 });
 
